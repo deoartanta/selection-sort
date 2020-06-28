0eab813a0add48a5d0e7d24eb66fb0993a2948e8
deoartanta <armadadeo064@gmail.com>
2020-06-28 16:16:15 +0700

    menghapus beberapa baris di showArry(), menukar posisi dengan selectionSort dan menambahkan beberapa baris di dalam selectionSort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -10,36 +10,32 @@ void swap(int &a, int &b)
     b = temp; 
 }
 
+void showArray(int arr[], int size)
+{ 
+    int i;
+    for (i=0; i < size; i++)
+	cout<<arr[i]<<", ";
+	cout<<endl<<endl;
+}
+
 void selectionSort(int arr[], int n) 
 { 
     int i, j, min_idx; 
     for (i = 0; i < n-1; i++) 
     { 
-        min_idx = i; 
+        min_idx = i;
         for (j = i+1; j < n; j++)
-          if (arr[j] < arr[min_idx]) 
-            min_idx = j; 
-        swap(arr[min_idx], arr[i]); 
+          if (arr[j] < arr[min_idx]){
+	          cout<<"Iterasi ke "<<i+1<<" : "<<endl;
+	          showArray(arr, n);
+	            min_idx = j; 
+	        swap(arr[min_idx], arr[i]); 
+		  }
     } 
 }
 
-void showArray(int arr[], int size, bool iterasi = false)
-{ 
-    int i;
-    if(iterasi){
-	    cout<<"Iterasi ke "<<i<<" : "<<endl;
-	    for (i=0; i < size; i++)
-		cout<<arr[i]<<", ";
-		cout<<endl;
-	}else{
-		for (i=0; i < size; i++)
-		cout<<arr[i]<<", ";
-		cout<<endl;
-	}
-}
-
 int main(){
-  	int arr[] = {5, 1, 12, -5, 16, 2, 12, 14};
+  	int arr[] = {5, 1, 12, 2, 12};
 	int n = sizeof(arr)/sizeof(arr[0]);
 	cout<<"PENGURUTAN DENGAN SELECTION SORT"<<endl;
     cout<<"================================"<<endl; 
@@ -48,6 +44,6 @@ int main(){
 	cout<<endl;
 	selectionSort(arr, n);
 	cout<<"Setelah dilakukan selection sort: "<<endl;
-	showArray(arr, n,true); 
+	showArray(arr, n); 
 	return 0;
 }
